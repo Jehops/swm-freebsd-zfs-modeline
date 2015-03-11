2ca6e1b23283ab7f7a63e377f1b1b68cefcc06fa
Joseph Mingrone <jrm@ftfl.ca>
2015-03-11 18:07:27 -0300

    Don't store shell commands in variables and don't use backticks

--- ml_zfs.sh
+++ ml_zfs.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-disk=ada0 ## customize this
+## customize these
+disk=ada0
 interval=3
-pool="tank" ## customize this
-stump_pid=`pgrep -a -n stumpwm`
-zfs_cmd="zpool list -Hp ${pool}"
+pool="tank"
+
+stump_pid=$(pgrep -a -n stumpwm)
 
 # while stumpwm is still running
 while kill -0 $stump_pid > /dev/null 2>&1; do
-    io_info=`iostat -c2 -w ${interval} -x ${disk} | awk 'NR%6==0 {print $4 " " $5}'`
+    io_info=$(iostat -c2 -w ${interval} -x ${disk} | \
+		     awk 'NR%6==0 {print $4 " " $5}')
     set -- ${io_info}
-    read=`scale=3; echo $1/1024 | bc -l`
-    write=`scale=3; echo $2/1024 | bc -l`
-    zfs_info=`eval ${zfs_cmd}`
+    read=$(scale=3; echo $1/1024 | bc -l)
+    write=$(scale=3; echo $2/1024 | bc -l)
+    zfs_info=$(zpool list -Hp ${pool})
     set -- $zfs_info
     # pool name used total free percent read write
     printf "%s %3.0f %3.0f %3.0f %3.0f %3.1f %3.1f\n"\
