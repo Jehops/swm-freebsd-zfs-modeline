9206982c672bd33699b47684efac8e72134c64b6
Joseph Mingrone <jrm@ftfl.ca>
2016-04-17 14:32:28 -0300

    trivial

--- ml_zfs.sh
+++ ml_zfs.sh
@@ -12,8 +12,8 @@ while kill -0 "$stump_pid" > /dev/null 2>&1; do
     io_info=$(iostat -c2 -w "$interval" -x "$disk" | \
 		     awk 'NR%6==0 {print $4 " " $5}')
     set -- ${io_info}
-    read=$(bc -e "scale=1;$1/1024" -e quit)
-    write=$(bc -e "scale=1;$2/1024" -e quit)
+    read=$( bc -e "scale=2;$1/1024" -e quit)
+    write=$(bc -e "scale=2;$2/1024" -e quit)
     zfs_info="$(zpool list -Hp "$pool")"
     set -- $zfs_info
     # pool name used total free percent read write
